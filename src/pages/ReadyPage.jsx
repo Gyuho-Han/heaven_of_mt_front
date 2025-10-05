b6dc60d0bf22e9e43c2a92e5bf4f82532f844b0f
jiwonpark831 <jiwonpark831@gmail.com>
2025-10-05 22:49:48 +0900

    style: 반응형 ready일때 UI 수정

--- src/pages/ReadyPage.jsx
+++ src/pages/ReadyPage.jsx
@@ -1,12 +1,18 @@
-
 import React from 'react';
+import styled from 'styled-components';
 
 const ReadyPage = () => {
-  return (
-    <div>
-      <h1>Please make your screen wider</h1>
-    </div>
-  );
+  return (<Container />);
 };
 
 export default ReadyPage;
+
+
+const Container = styled.div`
+  background-image: url('/images/ready.png'), url('/images/home.gif');
+  background-size: contain, cover;
+  width: 100vw;
+  height: 100vh;
+  background-position: center, center;
+  background-repeat: no-repeat, no-repeat;
+`;
